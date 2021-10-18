3fab80dde7f222d62a32aee385728ec41f34c1b1
sysadmin <sysadmin@ws-232-08.cl232.avalon.ru>
2021-10-18 21:38:40 +0300

    init myweb

--- myweb/myweb.c
+++ myweb/myweb.c
@@ -1,27 +1,34 @@
 #include <stdio.h>
 #include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/mman.h>
+
 
 #define HTTP_HEADER_LEN 256
 #define HTTP_REQUEST_LEN 256
 #define HTTP_METHOD_LEN 6
-#define HTTP_URI_LEN 8000
-#define HTTP_URI_PATH_LEN 1000
-#define HTTP_URI_PARAMS_LEN 7000
-#define HTTP_SERVER_LEN 36
+#define HTTP_URI_LEN 100
+#define FILE_NAME_LEN 1000
+#define LOG_ENTRY_LEN 1000
 
 #define REQ_END 100
 #define ERR_NO_URI -100
 #define ERR_ENDLESS_URI -101
 
+char const *index_file = "index.html";
+
 struct http_req {
 	char request[HTTP_REQUEST_LEN];
 	char method[HTTP_METHOD_LEN];
 	char uri[HTTP_URI_LEN];
-	char uri_path[HTTP_URI_PATH_LEN];
-	char uri_params[HTTP_URI_PARAMS_LEN];
+	char uri_path[HTTP_URI_LEN];
+	// uri_params
 	// version
 	// user_agent
-	char server[HTTP_SERVER_LEN];
+	// server
 	// accept
 };
 
@@ -30,9 +37,7 @@ int fill_req(char *buf, struct http_req *req) {
 		// пустая строка (\r\n) означает конец запроса
 		return REQ_END;
 	}
-	printf("<p>Input buf = </p>", buf, strlen(buf));
-	
-	char *p, *a, *b, *first_space, *sec_space, *third_space, *question;
+	char *p, *a, *b;
 	// Это строка GET-запроса
 	p = strstr(buf, "GET");
 	if (p == buf) {
@@ -43,24 +48,13 @@ int fill_req(char *buf, struct http_req *req) {
 		// и т.п.
 		strncpy(req->request, buf, strlen(buf));
 		strncpy(req->method, "GET", strlen("GET"));
-
 		a = strchr(buf, '/');
 		if ( a != NULL) { // есть запрашиваемый URI 
-			b = strchr(a, ' '); // URI ot a do b
+			b = strchr(a, ' ');
 			if ( b != NULL ) { // конец URI
 				strncpy(req->uri, a, b-a);
-				sec_space = strchr(a, ' ');
-				question = strchr(a, '?');
-				if ( question == NULL && sec_space != NULL) { // path = c - a
-					strncpy(req->uri_path, a, sec_space - a);
-				}
-				else if ( question != NULL ) {
-					strncpy(req->uri_path, a, question - a);
-					third_space = strchr(question, ' ');
-					strncpy(req->uri_params, question + 1, third_space - question);
-					return 0;
-				}
-				
+				// пусть пока URI_PATH - то же, что и URI
+				strncpy(req->uri_path, a, b-a);
 			} else {
 				return ERR_ENDLESS_URI;  
 				// тогда это что-то не то
@@ -71,39 +65,82 @@ int fill_req(char *buf, struct http_req *req) {
 		}
 	}
 
-	p = strstr(buf, "HOST");
-	if (p == buf)
-	{
-		strncpy(req->request, buf, strlen(buf));
-                strncpy(req->method, "HOST", strlen("HOST"));
-		first_space = strchr(p, ' ');
-		sec_space = strchr(first_space, ' ');
-		strncpy(req->server, first_space + 1, sec_space - first_space);
+	return 0;	
+}
+
+int log_req(char* log_path, struct http_req *req) {
+	int fd;
+	char log_entry[LOG_ENTRY_LEN] = "Sample Log Entry";
+	if ((fd = open(log_path, O_WRONLY | O_CREAT | O_APPEND, 0600)) < 0) {
+		perror(log_path);
+		return 1;
 	}
+	if (write(fd, log_entry, strlen(log_entry)) != strlen(log_entry)) {
+		perror(log_path);
+		return 1;
+	}
+        write(fd, "\n", 1);
+        fsync(fd);
+        close(fd);
+        return 0;
+}
 
+int make_resp(char *base_path, struct http_req *req) {
+        int fdin;
+        struct stat statbuf;
+        void *mmf_ptr;
+	// определяем на основе запроса, что за файл открыть
+	char res_file[FILE_NAME_LEN] = "";
+	if (base_path != NULL) {
+		strncpy(res_file,base_path,strlen(base_path));
+	}
+	strcat(res_file,index_file); // вот сюда писать отображение запроса в файловые пути 
+	// открываем
+        if ((fdin=open(res_file, O_RDONLY)) < 0) {
+                perror(res_file);
+                return 1;
+        }
+	// размер
+        if (fstat(fdin, &statbuf) < 0) {
+                perror(res_file);
+                return 1;
+        }
+	// mmf
+        if ((mmf_ptr = mmap(0, statbuf.st_size, PROT_READ, MAP_SHARED, fdin, 0)) == MAP_FAILED) {
+                perror("myfile");
+                return 1;
+        }
+	// Выводим HTTP-заголовки
+	char *http_result = "HTTP/1.1 200 OK\r\n";
+	write(1,http_result,strlen(http_result));
+	char *http_contype = "Content-Type: text/html\r\n";
+	write(1,http_contype,strlen(http_contype));
+	char *header_end = "\r\n";
+	write(1,header_end,strlen(header_end));
+	// Выводим запрошенный ресурс
+        if (write(1,mmf_ptr,statbuf.st_size) != statbuf.st_size) {
+                perror("stdout");
+                return 1;
+        }
+	// Подчищаем ресурсы
+        close(fdin);
+        munmap(mmf_ptr,statbuf.st_size);
 	return 0;
 }
 
-int log_req(struct http_req *req) {
-	// fprintf(stderr, "%s %s\n%s\n", req->request, req->method, req->uri);
-	return 0;
-}
-
-int make_resp(struct http_req *req) {
-	printf("HTTP/1.1 200 OK\r\n");
-	printf("Content-Type: text/html\r\n");
-	printf("\r\n");
-	printf("<html><body><title>Page title</title><h1>Page Header</h1>");
-	printf("<p>Request: %s", req->request);
-	printf("<p>Method: %s</p>", req->method);
-	printf("<p>URI Path %s</p>", req->uri_path);
-	printf("<p>URI Params %s</p>", req->uri_params);
-	printf("<p>Server %s", req->server);
-	printf("</body></html>\r\n");
-	return 0;
-}
-
-int main (void) {
+int main (int argc, char* argv[]) {
+	// первый параметр - каталог с контентом
+	// второй параметр - каталог для ведения журнала
+	char base_path[FILE_NAME_LEN] = "";
+	char log_path[FILE_NAME_LEN] = "";
+	char const *log_file = "access.log";
+	if ( argc > 2 ) { // задан каталог журнализации
+		strncpy(base_path, argv[1], strlen(argv[1]));
+		strncpy(log_path, argv[2], strlen(argv[2]));
+		strcat(log_path,"/");
+		strcat(base_path,"/");
+	}
+	strcat(log_path,log_file);
 	char buf[HTTP_HEADER_LEN];
 	struct http_req req;
 	while(fgets(buf, sizeof(buf),stdin)) {
@@ -119,6 +156,6 @@ int main (void) {
 			printf("Error: %d\n", ret);
 		
 	}
-	log_req(&req);
-	make_resp(&req);
+	log_req(log_path, &req);
+	make_resp(base_path,&req);
 }
